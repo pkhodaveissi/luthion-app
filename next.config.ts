0064e760dda865850816b9ae53754f14b71e99db
Pierre <pierreveissi@gmail.com>
2025-03-15 13:17:28 +0000

    chore: remove soon: enable prod logs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  productionBrowserSourceMaps: true, // Enable source maps in production
+  env: {
+    NEXT_PUBLIC_DEBUG: 'true',      // Enable debug mode
+  },
 };
 
 export default nextConfig;
